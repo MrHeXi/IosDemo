1af254e609166845cfbdc467ccb89f3fe646f9ad
hexi <hexi@21tb.com>
2016-04-05 21:42:44 +0800

    demo coding

--- Switch_Slider_SegmentedControl/Switch_Slider_SegmentedControl/ViewController.m
+++ Switch_Slider_SegmentedControl/Switch_Slider_SegmentedControl/ViewController.m
@@ -10,6 +10,11 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UISwitch *leftSwitch;
+
+@property (weak, nonatomic) IBOutlet UISwitch *rightSwitch;
+@property (weak, nonatomic) IBOutlet UILabel *sliderValue;
+
 @end
 
 @implementation ViewController
@@ -17,6 +22,30 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+}
+
+-(IBAction)switchValueChanged:(id)sender{
+    UISwitch *witchSwitch = (UISwitch*)sender;
+    BOOL setting = witchSwitch.isOn;
+    [self.leftSwitch setOn:setting animated:YES];
+    [self.rightSwitch setOn:setting animated:YES];
+}
+
+- (IBAction)sliderValueChanged:(id)sender {
+    UISlider *slider = (UISlider*)sender;
+    int progressAsInt = (int)(slider.value);
+    NSString *textValue = [[NSString alloc]initWithFormat:@"%d",progressAsInt];
+    self.sliderValue.text = textValue;
+    
+}
+- (IBAction)touchDown:(id)sender {
+    if (self.leftSwitch.hidden == YES) {
+        self.rightSwitch.hidden = NO;
+        self.leftSwitch.hidden = NO;
+    }else{
+        self.leftSwitch.hidden = YES;
+        self.rightSwitch.hidden = YES;
+    }
 }
 
 - (void)didReceiveMemoryWarning {
