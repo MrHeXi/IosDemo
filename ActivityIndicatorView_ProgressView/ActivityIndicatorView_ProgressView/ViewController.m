f89599a7f4a4d5e0da2e6d810b0de949d417c59d
hexi <hexi@21tb.com>
2016-04-06 10:04:33 +0800

    complete activityIndicator&progressView

--- ActivityIndicatorView_ProgressView/ActivityIndicatorView_ProgressView/ViewController.m
+++ ActivityIndicatorView_ProgressView/ActivityIndicatorView_ProgressView/ViewController.m
@@ -10,9 +10,14 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *myActivityIndicator;
+@property (weak, nonatomic) IBOutlet UIProgressView *myProgress;
+
 @end
 
-@implementation ViewController
+@implementation ViewController{
+    NSTimer *myTimer;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -23,5 +28,25 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+- (IBAction)activityBtnClick:(id)sender {
+    if ([self.myActivityIndicator isAnimating]) {
+        [self.myActivityIndicator stopAnimating];
+    }else{
+        [self.myActivityIndicator startAnimating];
+    }
+}
 
+- (IBAction)progressBtnClick:(id)sender {
+    self.myProgress.progress = 0.0;
+    myTimer = [NSTimer scheduledTimerWithTimeInterval:1.0 target:self selector:@selector(beginProgress) userInfo:nil repeats:YES];
+}
+
+-(void)beginProgress{
+    self.myProgress.progress=self.myProgress.progress+0.01;
+    if (self.myProgress.progress==1.0) {
+        [myTimer invalidate];
+        UIAlertView *alert = [[UIAlertView alloc]initWithTitle:@"progress complete!" message:@"" delegate:nil cancelButtonTitle:@"ok" otherButtonTitles:nil];
+        [alert show];
+    }
+}
 @end
