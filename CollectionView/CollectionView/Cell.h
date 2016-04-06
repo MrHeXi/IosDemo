429a3e492aad4efb1283da4b4771216c1bd401f9
hexi <hexi@21tb.com>
2016-04-06 20:19:15 +0800

    collectionView && UICollectionViewCel

---
+++ CollectionView/CollectionView/Cell.h
@@ -0,0 +1,13 @@
+//
+//  Cell.h
+//  CollectionView
+//
+//  Created by 何溪 on 16/4/6.
+//  Copyright © 2016年 何溪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Cell : UICollectionViewCell
+
+@end
---
+++ CollectionView/CollectionView/Cell.m
@@ -0,0 +1,13 @@
+//
+//  Cell.m
+//  CollectionView
+//
+//  Created by 何溪 on 16/4/6.
+//  Copyright © 2016年 何溪. All rights reserved.
+//
+
+#import "Cell.h"
+
+@implementation Cell
+
+@end
