29d6a336ec2f76daecacb0e035b3f764186b138c
hexi <hexi@21tb.com>
2016-04-07 00:00:17 +0800

    tableview

---
+++ TableView/CustomCell/CustomCell.h
@@ -0,0 +1,13 @@
+//
+//  CustomCell.h
+//  TableView
+//
+//  Created by 何溪 on 16/4/6.
+//  Copyright © 2016年 何溪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomCell : UICollectionViewCell
+
+@end
---
+++ TableView/CustomCell/CustomCell.m
@@ -0,0 +1,13 @@
+//
+//  CustomCell.m
+//  TableView
+//
+//  Created by 何溪 on 16/4/6.
+//  Copyright © 2016年 何溪. All rights reserved.
+//
+
+#import "CustomCell.h"
+
+@implementation CustomCell
+
+@end
